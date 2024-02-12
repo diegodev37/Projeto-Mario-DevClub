50dd1870821b3492787f9ed4fa54d9823ecb5ac7
diegodev37 <diego.dev.37@gmail.com>
2024-02-12 18:20:28 -0300

    Projeto Mario DevClub

---
+++ scripts.js
@@ -0,0 +1,24 @@
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+
+function mostrarform() {
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+
+}
+
+function esconderForm() {
+    form.style.left = "-300px"
+    form.style.transform = "translateX(0)"
+    mascara.style.visibility = "hidden"
+
+}
+
+
+
+
+
+
+
